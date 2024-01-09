c5ce8a5c7ae4f05bc8096bc2a31f06069294194d
JeongHyeon1997 <maszl951@naver.com>
2024-01-09 17:57:43 +0900

    lck4 api 생성

--- src/routes/lck4.ts
+++ src/routes/lck4.ts
@@ -5,5 +5,53 @@ const crypto = require("crypto");
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("hi");
+  res.send([
+    {
+      firstTeam: "Team 감자",
+      firstTeamWin: 0,
+      secondTeam: "Team 정현",
+      secondTeamWin: 0,
+      state: "진행",
+      startDate: "01.01(월)",
+      startTime: "17:00",
+    },
+    {
+      firstTeam: "Team 감자",
+      firstTeamWin: 1,
+      secondTeam: "Team 정현",
+      secondTeamWin: 0,
+      state: "종료",
+      startDate: "01.02(월)",
+      startTime: "17:00",
+    },
+    {
+      firstTeam: "Team 감자",
+      firstTeamWin: 0,
+      secondTeam: "Team 정현",
+      secondTeamWin: 1,
+      state: "예정",
+      startDate: "01.02(월)",
+      startTime: "17:00",
+    },
+    {
+      firstTeam: "Team 감자",
+      firstTeamWin: 2,
+      secondTeam: "Team 정현",
+      secondTeamWin: 1,
+      state: "종료",
+      startDate: "02.02(월)",
+      startTime: "17:00",
+    },
+    {
+      firstTeam: "Team 감자",
+      firstTeamWin: 0,
+      secondTeam: "Team 정현",
+      secondTeamWin: 0,
+      state: "종료",
+      startDate: "02.02(월)",
+      startTime: "17:00",
+    },
+  ]);
 });
+
+module.exports = router;
