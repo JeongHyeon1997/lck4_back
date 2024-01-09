ffe273e7c98e5c0d8a0148e6935e2ac781eee3f5
JeongHyeon1997 <maszl951@naver.com>
2024-01-09 22:22:40 +0900

    MySQL DB 연결 및 더미데이터 변경

--- src/routes/lck4.ts
+++ src/routes/lck4.ts
@@ -5,54 +5,12 @@ require("dotenv").config();
 const crypto = require("crypto");
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send([
-    {
-      firstTeam: "Team 감자",
-      firstTeamWin: 0,
-      secondTeam: "Team 정현",
-      secondTeamWin: 0,
-      state: "진행",
-      startDate: "01.01(월)",
-      startTime: "17:00",
-    },
-    {
-      firstTeam: "Team 감자",
-      firstTeamWin: 1,
-      secondTeam: "Team 정현",
-      secondTeamWin: 0,
-      state: "종료",
-      startDate: "01.02(월)",
-      startTime: "17:00",
-    },
-    {
-      firstTeam: "Team 감자",
-      firstTeamWin: 0,
-      secondTeam: "Team 정현",
-      secondTeamWin: 1,
-      state: "예정",
-      startDate: "01.02(월)",
-      startTime: "17:00",
-    },
-    {
-      firstTeam: "Team 감자",
-      firstTeamWin: 2,
-      secondTeam: "Team 정현",
-      secondTeamWin: 1,
-      state: "종료",
-      startDate: "02.02(월)",
-      startTime: "17:00",
-    },
-    {
-      firstTeam: "Team 감자",
-      firstTeamWin: 0,
-      secondTeam: "Team 정현",
-      secondTeamWin: 0,
-      state: "종료",
-      startDate: "02.02(월)",
-      startTime: "17:00",
-    },
-  ]);
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  const mysqlConnect = await mysql;
+  const [record] = await mysqlConnect.execute(`
+    SELECT * FROM RECORD
+  `);
+  res.send(record);
 });
 
 router.post("/lol", async (req: Request, res: Response, next: NextFunction) => {
--- src/utils/mysql.ts
+++ src/utils/mysql.ts
@@ -1,10 +1,10 @@
 import mysql2 from "mysql2/promise";
 
 const mysql = mysql2.createConnection({
-  host: "",
-  user: "",
-  password: "",
-  database: "",
+  host: "database-1.c8gvj9yivdfc.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "wpdwpd3dh0!",
+  database: "lck4",
 });
 
 export default mysql;
