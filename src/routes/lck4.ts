03e924f5f188968d78f970baa181d7a78db7a855
JeongHyeon1997 <maszl951@naver.com>
2024-01-19 21:53:03 +0900

    유저 데이터 업데이트 API 생성 match Query Update

--- src/routes/lck4.ts
+++ src/routes/lck4.ts
@@ -8,60 +8,69 @@ const crypto = require("crypto");
 const router = express.Router();
 
 router.post("/lol", async (req: Request, res: Response, next: NextFunction) => {
-  const userName = req.body.userName;
-  const mysqlConnect = await mysql;
-  const userInfo = await axios.get(
-    `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userName}?api_key=${process.env.LOL_API_KEY}`
-  );
-  const rankInfo = await axios.get(
-    `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${userInfo.data.id}?api_key=${process.env.LOL_API_KEY}`
-  );
-  console.log(rankInfo.data);
-
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-
-  await page.goto(`https://fow.kr/find/${userName}`);
-
-  const result = await page.evaluate(() => {
-    const contentContainer = document.querySelector(
-      "#content-container > div:nth-child(1) > div:nth-child(2) > div.topp > div.profile > div:nth-child(3)"
+  try {
+    const userName = req.body.userName;
+    const password = req.body.password;
+    const mysqlConnect = await mysql;
+    const userInfo = await axios.get(
+      `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userName}?api_key=${process.env.LOL_API_KEY}`
     );
-    return contentContainer.textContent;
-  });
+    const rankInfo = await axios.get(
+      `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${userInfo.data.id}?api_key=${process.env.LOL_API_KEY}`
+    );
 
-  await browser.close();
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
 
-  const highTier = getHighTier(result);
-  const tier = rankInfo.data.length === 0 ? "UNRANKED" : "UNRANKED";
-  const currentDate = new Date();
-  await mysqlConnect
-    .query(
-      `
-      INSERT INTO USER (
-        level
-        , name
-        , highTier
-        , tier
-        , win
-        , loss
-        , rate
-        , createDate
-      ) VALUES (
-        ${userInfo.data.summonerLevel}
-        , '${userInfo.data.name}'
-        , '${highTier}'
-        , '${tier}'
-        , 0
-        , 0
-        ,'0'
-        , '${currentDate.toISOString().slice(0, 19).replace("T", " ")}'
-      )
-  `
-    )
-    .then((data) => {
-      res.sendStatus(200);
+    await page.goto(`https://fow.kr/find/${userName}`);
+
+    const result = await page.evaluate(() => {
+      const contentContainer = document.querySelector(
+        "#content-container > div:nth-child(1) > div:nth-child(2) > div.topp > div.profile > div:nth-child(3)"
+      );
+      return contentContainer.textContent;
     });
+
+    await browser.close();
+
+    const highTier = getHighTier(result);
+    const tier = rankInfo.data.length === 0 ? "UNRANKED" : "UNRANKED";
+    const currentDate = new Date();
+    await mysqlConnect
+      .query(
+        `
+        INSERT INTO USER (
+          level
+          , name
+          , password
+          , highTier
+          , tier
+          , win
+          , loss
+          , rate
+          , updateDate
+          , createDate
+        ) VALUES (
+          ${userInfo.data.summonerLevel}
+          , '${userInfo.data.name}'
+          , '${highTier}'
+          , '${tier}'
+          , '${password}'
+          , 0
+          , 0
+          ,'0'
+          , null
+          , '${currentDate.toISOString().slice(0, 19).replace("T", " ")}'
+        )
+    `
+      )
+      .then((data) => {
+        res.sendStatus(200);
+      });
+  } catch (e) {
+    res.sendStatus(500);
+    throw e;
+  }
 });
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
@@ -123,7 +132,16 @@ router.get(
       LEFT OUTER JOIN USER u1 ON u1.id = m.firstTeamUserId
       LEFT OUTER JOIN USER u2 ON u2.id = m.secondTeamUserId
       WHERE
-        RecordId = ${id};
+        RecordId = ${id}
+        ORDER BY
+      CASE m.Line
+          WHEN 'TOP' THEN 1
+          WHEN 'JG' THEN 2
+          WHEN 'MID' THEN 3
+          WHEN 'AD' THEN 4
+          WHEN 'SUP' THEN 5
+          ELSE 6
+      END;
     `);
       res.send(match);
     } catch (e) {
@@ -149,6 +167,38 @@ router.get(
     `);
       res.send(record);
     } catch (e) {
+      throw e;
+    }
+  }
+);
+
+router.put(
+  "/user/update",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const mysqlConnect = await mysql;
+      const currentDate = new Date();
+      const [update] = await mysqlConnect.execute(`
+      UPDATE USER
+      SET
+        level = '${req.body.level}'
+        , name = '${req.body.name}'
+        , password = '${req.body.password}'
+        , highTier = '${req.body.highTier}'
+        , tier = '${req.body.tier}'
+        , win = '${req.body.win}'
+        , loss = '${req.body.loss}'
+        , rate = '${req.body.rate}'
+        , updateDate = '${currentDate
+          .toISOString()
+          .slice(0, 19)
+          .replace("T", " ")}'
+      WHERE
+        id = ${req.body.id}
+  `);
+      res.sendStatus(200);
+    } catch (e) {
+      res.sendStatus(500);
       throw e;
     }
   }
